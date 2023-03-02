93c12c494ba92a678efd3fcbcd2e838cfa86a193
getenesh <getenesh.guta@gmail.com>
2023-03-02 18:26:52 -0500

    Separate module

--- play.js
+++ play.js
@@ -1,22 +1,5 @@
-const net = require("net");
 
+const connect = require('./client')
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '192.168.0.27',
-    port: 50541,
-  });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("data", () => {
-    // code that does something when the connection is first established
-    console.log("Ended cuz you idled");
-  });
-
-  return conn; 
-};
-
-
-
 console.log("Connecting ...");
 connect();
