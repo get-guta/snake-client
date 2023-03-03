9f18fd3bafddf78368d59ea4de1aa5a42f3cef95
getenesh <getenesh.guta@gmail.com>
2023-03-02 20:59:58 -0500

    keyboard input hadling with server

--- play.js
+++ play.js
@@ -1,8 +1,7 @@
 
-const {connect} = require('./client')
-const {setupInput}= require('./input')
+const connect = require('./client')
+const setupInput= require('./input')
 // establishes a connection with the game server
 console.log("Connecting ...");
-connect();
 
-setupInput();
+setupInput(connect());
